d22ea695c82a55b5503b8654a6bb5ef65da14e5f
Alextokyocoders <khraxo95@gmail.com>
2018-09-27 09:03:46 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// req.query
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var usersRoute = require('./routes/user.route');
+var reviewsRoute = require('./routes/reviews.route');
+
+var port = 3000;
+
+var app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use(express.static('public'));
+
+app.get('/',function(req, res){
+  res.render('index', {
+    name: 'Alex'
+  });
+});
+
+app.use('/users', usersRoute);
+app.use('/reviews', reviewsRoute);
+
+app.listen(port, function () {
+  console.log('Server listening on port', + port);
+});
