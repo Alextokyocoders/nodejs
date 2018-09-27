1b2f4c6028b2c0062d1ed46a598da00249218642
Alextokyocoders <khraxo95@gmail.com>
2018-09-27 23:58:01 +0700

    add panination

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,3 +1,17 @@
 module.exports.index = function(req, res){
-  res.render('products/index');
+  var page = parseInt(req.query.page) || 1; // n
+  var perPage = 8; // x
+
+  var start = (page - 1) * perPage;
+  var end = page * perPage;
+
+  var drop = (page - 1) * perPage;
+
+  res.render('products/index', {
+    // cach 1:
+    // products: db.get('products').value().slice(start, end)
+    // cach 2:
+    products: db.get('products').drop(drop).take(perPage).value(),
+    page: page
+  });
 };
