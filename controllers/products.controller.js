f5747463984ca25a934930116bf03b6402d2d31b
Alextokyocoders <khraxo95@gmail.com>
2018-09-28 11:25:51 +0700

    add controller.search cho products ket hop vs pagination

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,3 +1,4 @@
+
 module.exports.index = function(req, res){
   var page = parseInt(req.query.page) || 1; // n
   var perPage = 8; // x
@@ -6,12 +7,42 @@ module.exports.index = function(req, res){
   var end = page * perPage;
 
   var drop = (page - 1) * perPage;
-
+  
+  var totalProducts = db.get('products').value();
+  var productsPerPage = db.get('products').drop(drop).take(perPage).value();
+  
   res.render('products/index', {
     // cach 1:
     // products: db.get('products').value().slice(start, end)
     // cach 2:
-    products: db.get('products').drop(drop).take(perPage).value(),
-    page: page
+    products: productsPerPage,
+    page: page,
+    total: totalProducts.length
+  });
+};
+
+module.exports.search = function (req, res) {
+  // Loc cac phan tu co tu khoa q
+  var q = req.query.q;
+
+  var matchedProducts = db.get('products').value().filter(function (product) {
+    return product.title.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+  });
+// Danh so trang moi page
+  var page = parseInt(req.query.page) || 1; // n
+  var perPage = 8; // x
+
+
+  var start = (page - 1) * perPage;
+  var end = page * perPage;
+//link goc
+  var baseUrl = '?q=' + q + '\&';
+// in ra cac phan tu
+  res.render('products/index', {
+    products: matchedProducts.slice(start, end),
+    keyWord: q,
+    page: page,
+    baseUrl: baseUrl,
+    total: matchedProducts.length
   });
 };
--- routes/products.route.js
+++ routes/products.route.js
@@ -6,4 +6,7 @@ var router = express.Router();
 
 router.get('/', controller.index);
 
+router.get('/search', controller.search);
+
 module.exports = router;
+
